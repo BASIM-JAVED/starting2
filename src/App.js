9fb46d59135fc0d293f4e09f2ffa49990f27bdd7
BASIM-JAVED <basimjaved@hotmail.com>
2023-05-29 11:01:33 +0500

    Weather

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState } from 'react';
+// // import MyComponent from './MyComponent';
+// import data from '../src/assests/data.json'
+// import Practice from './component/Practice';
+// import MyComponent from './component/Lop';
+// import Lop from './component/Lop';
+// import WeatherComponent from './component/WeatherComponent';
 
-function App() {
+// function App({country}) {
+// //   const [db, setData] = useState(data);
+// // console.log(db)
+//   return (
+//     <div>
+//       <h1>hgebuhbshj</h1>
+//       {/* <Practice>
+
+//       </Practice> */}
+//       {/* <MyComponent>
+        
+//       </MyComponent> */}
+//       {/* <Practice>
+
+//       </Practice> */}
+// {/* <Lop>
+
+// </Lop> */}
+
+
+// <WeatherComponent country={country} />
+//       {/* {data.map(item => (
+//         <MyComponent key={item.id} name={item.name} />
+//       ))} */}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
+import WeatherComponent from './component/WeatherComponent';
+
+const App = () => {
+  const [country, setCountry] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Call the API or perform any other logic here using the "country" state value
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+          placeholder="Enter a country"
+        />
+        <button type="submit">Get Weather</button>
+      </form>
+      <WeatherComponent country={country} />
     </div>
   );
-}
+};
 
 export default App;
