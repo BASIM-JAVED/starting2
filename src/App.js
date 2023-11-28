7b088c8b7fd1ff22e55503cf8df0d54dc89b69b1
BASIM-JAVED <basim.63297@iqra.edu.pk>
2023-11-28 14:33:38 +0500

    weather

--- src/App.js
+++ src/App.js
@@ -1,66 +1,67 @@
-// import React, { useState } from 'react';
-// // import MyComponent from './MyComponent';
-// import data from '../src/assests/data.json'
-// import Practice from './component/Practice';
-// import MyComponent from './component/Lop';
-// import Lop from './component/Lop';
-// import WeatherComponent from './component/WeatherComponent';
+import React, { useState } from 'react'
+import axios from 'axios'
 
-// function App({country}) {
-// //   const [db, setData] = useState(data);
-// // console.log(db)
-//   return (
-//     <div>
-//       <h1>hgebuhbshj</h1>
-//       {/* <Practice>
+function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
 
-//       </Practice> */}
-//       {/* <MyComponent>
-        
-//       </MyComponent> */}
-//       {/* <Practice>
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
 
-//       </Practice> */}
-// {/* <Lop>
-
-// </Lop> */}
-
-
-// <WeatherComponent country={country} />
-//       {/* {data.map(item => (
-//         <MyComponent key={item.id} name={item.name} />
-//       ))} */}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React, { useState } from 'react';
-import WeatherComponent from './component/WeatherComponent';
-
-const App = () => {
-  const [country, setCountry] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Call the API or perform any other logic here using the "country" state value
-  };
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
 
   return (
-    <div className='App'>
-      <form onSubmit={handleSubmit}>
+    <div className="app">
+      <div className="search">
         <input
-          type="text"
-          value={country}
-          onChange={(e) => setCountry(e.target.value)}
-          placeholder="Enter a country"
-        />
-        <button type="submit">Get Weather</button>
-      </form>
-      <WeatherComponent country={country} />
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text" />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }
+
+
+
+      </div>
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/component/Lop.js
@@ -0,0 +1,28 @@
+// import React, { useState } from 'react';
+// import data from '../assests/data.json';
+
+
+
+// function Lop  () {
+//   const [numbers, setNumbers] = useState(data);
+
+//   // console.log(numbers);
+//   return (
+    
+//     <div>
+//       {numbers.forEach ((number) => {
+//          <div >
+//          <li key={number.id}>{number.task}</li>
+          
+//         </div>
+//         // Perform operations on each item of the JSON data
+//         return (
+         
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+// export default Lop;
+
